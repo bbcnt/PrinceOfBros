467cbd15191d089ce3280667612c359964e89451
GregoireDec <gregoire.decorvet@heig-vd.ch>
2014-06-17 10:44:23 +0200

    Actions de déplacement

---
+++ Programme/src/game/controlls/actions/MoveLeft.java
@@ -0,0 +1,51 @@
+/* ============================================================================
+ * Filename   : MoveLeft.java
+ * ============================================================================
+ * Created on : 17 juin 2014
+ * ============================================================================
+ * Authors    : Brito Carvalho Bruno
+ *              Decorvet Grégoire
+ *              Ngo Quang Dung
+ *              Schweizer Thomas
+ * ============================================================================
+ */
+package game.controlls.actions;
+
+import engine.GameController;
+import engine.models.player.Player;
+import game.controlls.PlayerControl;
+
+/**
+ * TODO
+ * @author Brito Carvalho Bruno
+ * @author Decorvet Grégoire
+ * @author Ngo Quang Dung
+ * @author Schweizer Thomas
+ *
+ */
+public class MoveLeft extends UserAction {
+
+	private Player player;
+	
+   public MoveLeft(PlayerControl controls, Player player) {
+	   super(controls);
+	   this.player = player;
+   }
+   
+   public Player getPlayer() {
+   	return player;
+   }
+
+	@Override
+   public boolean isAllowed() {
+		return player.getX() > 3 && GameController.getInstance().getWorld().getTile(
+				(int)Math.floor(player.getX() - 1), (int)Math.floor(player.getY()))
+				!= null;
+   }
+
+	@Override
+   public void execute() {
+	   getPlayerControl().moveLeft();
+   }
+
+}
---
+++ Programme/src/game/controlls/actions/MoveRight.java
@@ -0,0 +1,52 @@
+/* ============================================================================
+ * Filename   : MoveRight.java
+ * ============================================================================
+ * Created on : 17 juin 2014
+ * ============================================================================
+ * Authors    : Brito Carvalho Bruno
+ *              Decorvet Grégoire
+ *              Ngo Quang Dung
+ *              Schweizer Thomas
+ * ============================================================================
+ */
+package game.controlls.actions;
+
+import engine.GameController;
+import engine.models.player.Player;
+import game.controlls.PlayerControl;
+
+/**
+ * TODO
+ * @author Brito Carvalho Bruno
+ * @author Decorvet Grégoire
+ * @author Ngo Quang Dung
+ * @author Schweizer Thomas
+ *
+ */
+public class MoveRight extends UserAction {
+
+	private Player player;
+	
+   public MoveRight(PlayerControl controls, Player player) {
+	   super(controls);
+	   this.player = player;
+   }
+   
+   public Player getPlayer() {
+   	return player;
+   }
+
+	@Override
+   public boolean isAllowed() {
+		return player.getX() < GameController.getInstance().getWorld().getWidth() - 16
+				&& GameController.getInstance().getWorld().getTile(
+				(int)Math.floor(player.getX() + 1), (int)Math.floor(player.getY()))
+				!= null;
+   }
+
+	@Override
+   public void execute() {
+	   getPlayerControl().moveRight();
+   }
+
+}
